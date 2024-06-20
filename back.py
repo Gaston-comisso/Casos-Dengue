90b2235909784ebd7d6b7f432457a547c66e98c3
franco castillo <francoantonioredondo@gmail.com>
2024-06-19 22:41:32 -0300

    creacion de base de datos agregamos rutas para llenar el formulario, y la carga en la bdd modificacion del html inicio

--- back.py
+++ back.py
@@ -9,7 +9,12 @@ app = Flask(__name__)
 # Cargar variables de entorno desde un archivo .env
 load_dotenv()
 
-
+#Conexion a BDD
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') 
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') 
+mysql = MySQL(app)
 
 #rutas
 @app.route("/")
@@ -24,6 +29,28 @@ def regitro():
 def inicio():
     return render_template("inicio.html")
 
+
+#carga de casos
+@app.route("/cargadecasos", methods=['POST'])
+def carga_de_casos():
+    if  request.method == 'POST':
+        telefono = request.form['telefono']
+        nombre = request.form['nombre']
+        apellido= request.form['apellido']
+        barrio= request.form['barrio']
+        direccion= request.form['direccion']
+        tipo_de_caso= request.form['TipoDecaso']
+        
+        cur = mysql.connection.cursor()    
+        
+        cur.execute('''INSERT INTO grupos (telefono, nombre, apellido, barrio, direccion,casos)
+                    VALUES (%s, %s, %s, %s,%s,%s)'''
+                    , (telefono,nombre,apellido,barrio,direccion,tipo_de_caso))
+        
+        mysql.connection.commit()
+        
+        
+        return render_template ('inicio.html')
 
 
 
